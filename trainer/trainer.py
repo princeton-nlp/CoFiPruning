8fb6165d2640568868e3f2acdb9b2e8de375c269
learngit <zhangzhenyu13@outlook.com>
2022-07-01 16:44:11 +0800

    Update trainer.py
    create optimizer for avoiding adamw errors
    https://github.com/princeton-nlp/CoFiPruning/issues/10

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -111,7 +111,7 @@ class CoFiTrainer(Trainer):
         logging.set_verbosity(log_level)
         logger.setLevel(log_level)
 
-    def create_optimizer_and_scheduler(self, num_training_steps: int):
+    def create_optimizer_and_scheduler(self, num_training_steps: int, build_l0_optimizer:bool=False):
         def log_params(param_groups, des):
             for i, grouped_parameters in enumerate(param_groups):
                 logger.info(
@@ -140,7 +140,7 @@ class CoFiTrainer(Trainer):
                 eps=self.args.adam_epsilon,
             )
 
-            if self.l0_module is not None:
+            if build_l0_optimizer and self.l0_module is not None:
                 l0_params = [{
                     "params": [p for n, p in self.l0_module.named_parameters() if "lambda" not in n],
                     "weight_decay": 0.0,
@@ -195,7 +195,7 @@ class CoFiTrainer(Trainer):
             num_train_epochs = self.args.num_train_epochs
             self.args.max_steps = self.t_total
 
-        self.create_optimizer_and_scheduler(num_training_steps=self.t_total)
+        self.create_optimizer_and_scheduler(num_training_steps=self.t_total, build_l0_optimizer = self.start_prune)
 
         model = self.model
 
